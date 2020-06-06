2401d6587c9700ca8564123fc899f76f0a79840c
MenshikovIA <62717964+MenshikovIA@users.noreply.github.com>
2020-06-07 01:42:55 +0300

    Update settings.py

--- MathSite/settings.py
+++ MathSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ypo&%qakgcq1weudo$&7!$r7k16u+2sts+9g-pix@gn9&zpi)o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'imenshikov.pythonanywhere.com', ]
 
